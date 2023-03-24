09e5a343657084d7711d788c439849d7f4f1b606
max <maxzhang1119@gmail.com>
2023-03-24 13:17:49 -0400

    got rid of exam class, added add_patient and add_exam

--- Classes.py
+++ Classes.py
@@ -1,28 +1,18 @@
 from dataclasses import dataclass
 
 @dataclass
-class Exam:
-    examID: int
-
-    def __init__(self, examID: int):
-        self.examID = examID
-
-    def __str__(self):
-        return f"{self.examID}"
-
-@dataclass
 class Patient:
     patientID: int
     name: str
     exams: set()
 
     def __init__(self, patID: int, name: str):
-        self.patientID = patID
+        self.patID = patID
         self.name = name
         self.exams = set()
 
     def __str__(self):
-        return f"Name: {self.name}, Id: {self.patientID}, Exam Count: {len(self.exams)}"
+        return f"Name: {self.name}, Id: {self.patID}, Exam Count: {len(self.exams)}"
 
 class PatientPortal:
     patientDict: {}
@@ -31,6 +21,25 @@ class PatientPortal:
     def __init__(self):
         self.patientDict = {}
         self.examToPatientDict = {}
+
+
+    def add_patient(self, patID: int, name: str):
+        #if patient with given identifier exists, move on
+        if patID in self.patientDict: return
+        #otherwise add to dict
+        self.patientDict[patID] = Patient(patID, name)
+
+    def add_exam(self, patID: int, examID: int):
+        #if patient doesn't exist, move on
+        if patID not in self.patientDict: return
+        # if exam already exists, move on
+        if examID in self.examToPatientDict: return
+        #use this dict to track examID to patID relation in case of deletion
+        self.examToPatientDict[examID] = patID
+        #add exam to patient class with given patID
+        self.patientDict[patID].exams.add(examID)
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import Classes
+from Classes import PatientPortal
 
+def main():
+    portal = PatientPortal()
+
+    portal.add_patient(123, "John Doe")
+    portal.add_exam(123, 456)
+
+
+if __name__ == "__main__":
+    main()
