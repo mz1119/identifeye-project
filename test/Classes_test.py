b2c848e7276f19bdbc8d279b7414590292c3d118
max <maxzhang1119@gmail.com>
2023-03-24 23:32:37 -0400

    more tests cases

--- test/Classes_test.py
+++ test/Classes_test.py
@@ -2,6 +2,7 @@ import sys
 import os
 import pytest
 
+#importing from parent directory
 current = os.path.dirname(os.path.realpath(__file__))
 parent = os.path.dirname(current)
 sys.path.append(parent)
@@ -9,12 +10,57 @@ sys.path.append(parent)
 from Classes import PatientPortal
 
 
-def test1():
+def test_add_patient():
     portal = PatientPortal()
 
     #adding patient with same ID
     portal.add_patient(123, "John Doe")
     portal.add_patient(123, "Not John Doe")
 
+    assert str(portal.patientDict[123]) == "Name: John Doe, Id: 123, Exam Count: 0"
+
+def test_add_exam():
+    portal = PatientPortal()
+
+    #adding exam for nonexistent patient
+    portal.add_exam(123, 456)
+    assert portal.patientDict == {}
+
+    #adding exam for existing patient
+    portal.add_patient(123, "John Doe")
+    portal.add_exam(123, 456)
+
+    assert str(portal.patientDict[123]) == "Name: John Doe, Id: 123, Exam Count: 1"
+
+    #adding exam with same identifier to different person shouldn't do anything
+    portal.add_patient(789, "Mary Jane")
+    portal.add_exam(789,456)
+
+    assert str(portal.patientDict[789]) == "Name: Mary Jane, Id: 789, Exam Count: 0"
+
+def test_delete_patient():
+    portal = PatientPortal()
+
+    #deleting nonexistent patient
+    portal.delete_patient(123)
+    assert portal.patientDict == {}
+
+    #deleting existing patient
+    portal.add_patient(123, "John Doe")
+    assert str(portal.patientDict[123]) == "Name: John Doe, Id: 123, Exam Count: 0"
+    portal.delete_patient(123)
+    assert portal.patientDict == {}
+
+def test_delete_exam():
+    portal = PatientPortal()
+
+    #delete from empty list
+    portal.delete_exam(123)
+    assert portal.patientDict == {}
+
+    portal.add_patient(123, "John Doe")
+    portal.add_exam(123, 456)
+    assert str(portal.patientDict[123]) == "Name: John Doe, Id: 123, Exam Count: 1"
+    portal.delete_exam(456)
     assert str(portal.patientDict[123]) == "Name: John Doe, Id: 123, Exam Count: 0"
 
